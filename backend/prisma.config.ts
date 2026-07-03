8031513cef94e53f340dc5d25c92f8ab180f566b
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-07-03 11:05:09 +0530

    Prisma Setup

---
+++ backend/prisma.config.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+
+const envFile =
+  process.env.NODE_ENV == "production" ? ".env.production" : ".env.development";
+
+dotenv.config({
+  path: path.resolve(__dirname, envFile),
+});
+
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
