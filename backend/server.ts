84d155eebeef554b632cdc8aa19b9f8c18e6a75e
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-07-03 13:49:31 +0530

    added utility helpers for: prisma, bcrypt, jwt

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,14 @@
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import { config } from "./src/config/env";
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: config.env === "production" ? ["https://mydomain.com"] : "*",
+    credentials: true,
+  }),
+);
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -15,8 +15,8 @@ export const config = {
     url: process.env.DATABASE_URL,
   },
   jwt: {
-    accessSecret: process.env.JWT_ACCESS_SECRET,
-    refreshSecret: process.env.JWT_REFRESH_SECRET,
+    accessSecret: process.env.JWT_ACCESS_SECRET!,
+    refreshSecret: process.env.JWT_REFRESH_SECRET!,
     accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
     refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
   },
