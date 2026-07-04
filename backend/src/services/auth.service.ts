e772c43f774251f43c23f37bc7c68227df80137c
AjayRajNegi <ajayrajnegi111@gmail.com>
2026-07-04 09:01:51 +0530

    authService for registering User

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,78 @@
+import { hashedPassword } from "../utils/bcrypt";
+import { generateAccessToken, generateRefreshToken } from "../utils/jwt";
+import { logger } from "../utils/logger";
+import { prisma } from "../utils/prisma";
+
+interface RegisterInput {
+  email: string;
+  phone: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
+interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export const authService = {
+  async register(input: RegisterInput) {
+    const existing = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: input.email }, { phone: input.phone }],
+      },
+    });
+
+    if (existing) {
+      if (existing.email === input.email) {
+        throw new Error("Email already registered.");
+      }
+      throw new Error("Phone number already registered.");
+    }
+
+    const passwordHash = await hashedPassword(input.password);
+
+    const user = await prisma.user.create({
+      data: {
+        email: input.email,
+        phone: input.phone,
+        passwordHash,
+        firstName: input.firstName,
+        lastName: input.lastName,
+      },
+      select: {
+        id: true,
+        email: true,
+        phone: true,
+        firstName: true,
+        lastName: true,
+        role: true,
+        isVerified: true,
+      },
+    });
+
+    const tokenPayload = {
+      userId: user?.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    const accessToken = generateAccessToken(tokenPayload);
+    const refreshToken = generateRefreshToken(tokenPayload);
+
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 7);
+
+    await prisma.refreshToken.create({
+      data: {
+        token: refreshToken,
+        userId: user.id,
+        expiresAt,
+      },
+    });
+
+    logger.info(`New User Registered: ${user.email}`);
+    return { user, accessToken };
+  },
+};
